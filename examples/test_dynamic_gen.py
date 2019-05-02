77ca74d796b0fc5a0c56f2c2c9250e72d44dfe2f
jianlins <jianlinshi.cn@gmail.com>
2019-05-02 11:25:22 -0600

    clean up codes

--- examples/test_dynamic_gen.py
+++ examples/test_dynamic_gen.py
@@ -1,11 +1,9 @@
-from ncls import NCLSD
+from ncls import NCLS
 
-import pickle
-import pandas as pd
 import numpy as np
 
 
-ncls = NCLSD()
+ncls = NCLS()
 
 ncls.append(1,2,1)
 ncls.append(3,4,3)
@@ -13,14 +11,14 @@ ncls.append(5,7,5)
 
 ncls.build()
 
-starts2 = np.array([1,7], dtype=np.long)
-ends2 = np.array([2,8], dtype=np.long)
-indexes2 = np.array([11,22], dtype=np.long)
+starts2 = np.array([1,7])
+ends2 = np.array([2,8])
+indexes2 = np.array([11,22])
 
 print(ncls.has_overlaps(starts2, ends2, indexes2))
 
 print(ncls.intervals())
-# print(ncls.find_overlap(1,3))
+print(ncls.find_overlap(1,3))
 
 for i in ncls.find_overlap(1,4):
     print(i)
--- ncls/__init__.py
+++ ncls/__init__.py
@@ -6,6 +6,11 @@ import numpy as np
 
 def NCLS(starts=None, ends=None, ids=None):
     if starts is None or ends is None or ids is None:
+        test = np.array([1])
+        if test.dtype == np.int64:
+            return NCLS64()
+        elif test.dtype == np.int32:
+            return NCLS32()
         return NCLS64()
     if starts.dtype == np.int64:
         return NCLS64(starts, ends, ids)
@@ -13,9 +18,12 @@ def NCLS(starts=None, ends=None, ids=None):
         return NCLS32(starts, ends, ids)
 
 
-
 def NCLS32():
     return NCLS32()
+
+
+def NCLS64():
+    return NCLS64()
 
 
 from ncls.version import __version__
