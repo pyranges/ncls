7a1a6fcad4992370a452b2d4f1c77d4d758ddbb1
jianlins <jianlinshi.cn@gmail.com>
2019-05-02 11:21:22 -0600

    clean up codes

--- ncls/__init__.py
+++ ncls/__init__.py
@@ -4,17 +4,18 @@ from ncls.src.ncls32 import NCLS32
 import numpy as np
 
 
-def NCLS(starts, ends, ids):
+def NCLS(starts=None, ends=None, ids=None):
+    if starts is None or ends is None or ids is None:
+        return NCLS64()
     if starts.dtype == np.int64:
         return NCLS64(starts, ends, ids)
     elif starts.dtype == np.int32:
         return NCLS32(starts, ends, ids)
-    else:
-        return NCLS32()
 
 
-def NCLSD():
-    return NCLS64()
+
+def NCLS32():
+    return NCLS32()
 
 
 from ncls.version import __version__
