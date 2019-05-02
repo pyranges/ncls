8d419a0fd809800400f828a0f66baa4b186f93c1
Jianlin Shi <jianlins@users.noreply.github.com>
2019-05-02 11:42:34 -0600

    Update __init__.py

--- ncls/__init__.py
+++ ncls/__init__.py
@@ -7,11 +7,11 @@ import numpy as np
 def NCLS(starts=None, ends=None, ids=None):
     if starts is None or ends is None or ids is None:
         test = np.array([1])
-        if test.dtype == np.int64:
-            return NCLS64()
-        elif test.dtype == np.int32:
+        if test.dtype == np.int32:
             return NCLS32()
-        return NCLS64()
+        else:
+            return NCLS64()
+        
     if starts.dtype == np.int64:
         return NCLS64(starts, ends, ids)
     elif starts.dtype == np.int32:
