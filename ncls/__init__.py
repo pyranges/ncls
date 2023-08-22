618f7c852c76158d0b114b0e0ffc1785686cbe22
Joren Retel <joren.retel@pfizer.com>
2023-08-22 14:22:22 +0200

    migrated to importlib_metadata to replace pkg_resources is deprecated

--- ncls/__init__.py
+++ ncls/__init__.py
@@ -1,7 +1,7 @@
 import numpy as np
 import importlib_metadata
 
-__version__ = importlib_metadata.get_distribution("ncls").version
+__version__ = importlib_metadata.version("ncls")
 
 from ncls.src.ncls import NCLS64  # type: ignore
 from ncls.src.ncls32 import NCLS32  # type: ignore
